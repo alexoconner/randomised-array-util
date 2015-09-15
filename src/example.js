9a469affb42dc7476282a5ed5ccadf4b3abab11f
Alexander Skrabl <alexander@skrabl.de>
2015-09-15 22:23:37 +0100

    added html-minify and example html and js file

---
+++ src/example.js
@@ -0,0 +1,15 @@
+import RandomisedArrayUtil from './randomised-array';
+
+console.log('example js');
+
+var testArray = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g'
+];
+
+console.log(new RandomisedArrayUtil(testArray));
